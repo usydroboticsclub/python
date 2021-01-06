47619dc49067d35c538e2b6d13c7479a2dc089b7
Rishi Rangarajan <61214686+rran9235@users.noreply.github.com>
2021-01-06 14:12:13 +0530

    Fixes to variables - currentPosition
    Intialise currentPosition to 0 at the start. Increment currenPosition value in while loop. Set currentPosition to length of list.

--- 6_lists.py
+++ 6_lists.py
@@ -1,33 +1,36 @@
 # Say we want to store a lot of numbers at a time. We could use a lot of 
 # variables:
-print ("Input number 1:")
+print("Input number 1:")
 var1 = input()
 
-print ("Input number 2:")
+print("Input number 2:")
 var2 = input()
 
-print ("Input number 3:")
+print("Input number 3:")
 var3 = input()
 
-print ("Input number 4:")
+print("Input number 4:")
 var4 = input()
 
 # You can see how it's getting a little tedious. Instead, we can create a 
 # list of variables under the same name to do the same thing:
 
-listOfNumbers=[]
+print("Use a list instead to take input!")
+listOfNumbers = []
+currentPosition = 1
 
-while len(listOfNumbers)<5:
-  print ("Input number "+str(currentPosition)+":")
+while (len(listOfNumbers) < 5):
+  print("Input number "+str(currentPosition)+":")
+  currentPosition = currentPosition + 1
   listOfNumbers.append(input())
 
 # Another advantage of lists is that we can manipulate them in different ways:
 
-print ("Numbers in reverse order:")
+print("Numbers in reverse order:")
 
-currentPosition=len(listOfNumbers) -1
-while currentPosition>0:
-  currentPosition=currentPosition-1
+currentPosition = len(listOfNumbers)
+while (currentPosition > 0):
+  currentPosition = currentPosition - 1
   print(listOfNumbers[currentPosition])
  
 # (You may hear other programmers calling this an 'array'. Python likes to call
