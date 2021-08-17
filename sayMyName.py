b5363b963cb10ced396b133c07bb02709e198dc1
Edward Liang <edward.liang66@hotmail.com>
2021-08-17 14:16:22 +1000

    added myname file

---
+++ sayMyName.py
@@ -0,0 +1,5 @@
+name = input('Please type in your name: \n')
+multiply = int(input('please type in the first 3 numbers of your SID: \n'))
+
+for i in range(multiply):
+    print('Iteration number {}: {}'.format(i + 1, name))
